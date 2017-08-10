3a06f3b7fe4ea922a6254e0b4609673da37359b7
Hassan Hibbert <hhibbert@fullsail.com>
2017-08-10 11:17:41 -0400

    update version

--- validate.manager.js
+++ validate.manager.js
@@ -1,7 +1,7 @@
 /**
  * @preserve
  * ValidateManager: A JavaScript form validator
- * Version: 1.0.9
+ * Version: 1.0.10
  * Author: Hassan Hibbert <http://hassanhibbert.com/>
  * Github: https://github.com/hassanhibbert/validate-manager
  * Copyright 2016-2017 Hassan Hibbert, under the MIT License
